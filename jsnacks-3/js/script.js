bd5951e677d3aeaf75f5e7ebc30a4cbe3d609b1e
Mattia <matpolli93@gmail.com>
2021-03-04 13:04:24 +0100

    Code improved

--- jsnacks-3/js/script.js
+++ jsnacks-3/js/script.js
@@ -1,19 +1,19 @@
-// Starting from two arrays, create a false list comprising three guests' names and surnames
+// Starting from two arrays, create a false list comprising the three guests' names and surnames
 // Variables and Arrays
 var names = ["Luke", "Harry", "Frodo"];
 var surnames = ["Skywalker", "Potter", "Baggins"];
-var randomName = names[Math.floor(Math.random() * names.length)];
-var randomSurname = surnames[Math.floor(Math.random() * surnames.length)];
-
-// New Array
 var falseNamesList = [];
 var falseName;
 
 // Calculating
-for (var i = 1; i <= names.length && i <= surnames.length; i++) {
-    // Displaying
-    falseName = randomName + " " + randomSurname;
+for (var i = 1; i <= 3; i++) {
+    // Random
+    var randomName = Math.floor(Math.random() * names.length);
+    var randomSurname = Math.floor(Math.random() * surnames.length);
+    // Complete false names
+    falseName = (names[randomName] + " " + surnames[randomSurname]);
     falseNamesList.push(falseName);
 }
 
+// Displaying
 console.log(falseNamesList);
